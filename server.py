7cc83550d50a6073d679a6a4a5628e0232d44377
0 <75130619+iamprim0rdial@users.noreply.github.com>
2025-02-12 21:05:57 -0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,7 @@
+# Simple HTTP server to simulate a server under attack (for testing)
+def start_simple_server():
+    from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+    server = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
+    logging.info("Simple HTTP server started at http://localhost:8080")
+    server.serve_forever()
